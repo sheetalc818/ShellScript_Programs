0c741eaa3eb819b45a964100b5e3abff21ec145d
sheetalc818 <46039938+sheetalc818@users.noreply.github.com>
2019-12-27 12:40:04 +0530

    Added : Fixed Indentation

--- CaseSelectionPrograms/WeekDay.sh
+++ CaseSelectionPrograms/WeekDay.sh
@@ -6,7 +6,7 @@ function getDay()
 	case $1 in 
 				0) echo "SUNDAY" ;;
 				1) echo "MONDAY" ;;
-				2)	echo "TUESDAY" ;;
+				2) echo "TUESDAY" ;;
 				3) echo "WEDNESDAY" ;;
 				4) echo "THURSDAY" ;;
 				5) echo "FRIDAY" ;;
